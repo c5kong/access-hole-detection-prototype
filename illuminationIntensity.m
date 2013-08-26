c395da2d37ea7e58ef6284dafc5b0b6150c81495
Christopher Kong <cgkong@gmail.com>
2013-08-26 19:53:41 -0400

    AvgLum (in progress)

--- illuminationIntensity.m
+++ illuminationIntensity.m
@@ -21,13 +21,18 @@
 	%//=======================================================================
 	%// convert from RGB to YUV
 	%//=======================================================================
+	YCBCR = rgb2ycbcr(img);
+	Y = YCBCR(:, :, 1);
+	
+	%back to RGB
+	%RGB = ycbcr2rgb(YCBCR);
+	%figure, imshow(RGB);
 	
 	
-
 	%//=======================================================================
 	%// Find Region
 	%//=======================================================================	
-	figure, imshow(img, []), axis off, hold on;
+	figure, imshow(img), axis off, hold on;
 	b=0;
 	x1=0; x2=0;
 	y1=0; y2=0;
@@ -41,6 +46,20 @@
 		if b ==27
 			break;
 		end		
+		
+		x1=uint64(x1);
+		y1=uint64(y1);
+		x2=uint64(x2);
+		y2=uint64(y2);		
+		
+		sum = uint64(0);
+		for i=y1:y2
+			for j=x1:x2	
+				sum= uint64(Y(i, j))+ sum;
+			end			
+		end
+		
+		avgLum = sum/((x2-x1)+(y2-y1));
 		
 		
 	end
