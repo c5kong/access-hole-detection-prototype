4d0ba7a22758593b1e2f2cfc7672a6420d3e2d9f
Christopher Kong <cgkong@gmail.com>
2013-09-05 14:35:55 -0400

    fixed collectGroundTruth

--- groundTruth.m
+++ groundTruth.m
@@ -19,14 +19,20 @@ function [ X ] = groundTruth(imageName, directory)
 	%//=======================================================================
 	%figure, imshow(labels, []), colormap(gray), axis off, hold on;
 	figure, imshow(img, []), colormap(gray), axis off, hold on;
-	[x y b] = ginput(2);
 	
-	if b ~= 0
+	b=[];
+	x1=[]; y1=[];
+	x2=[]; y2=[];
+	
+	[x1 y1 b] = ginput(1);
+	
+	if b ~= 27 		
+		[x2 y2 b] = ginput(1);		
 		 M{1, 1} = imageName;
-		 M{1, 2} = x(1);
-		 M{1, 3} = y(1);
-		 M{1, 4} = abs(x(2)-x(1));
-		 M{1, 5} = abs(y(2)-y(1));	
+		 M{1, 2} = x1;
+		 M{1, 3} = y1;
+		 M{1, 4} = abs(x2-x1);
+		 M{1, 5} = abs(y2-y1);	
 		dlmcell(filename, M, ',', '-a');
 		rectangle('Position',[M{1, 2} M{1, 3}  M{1, 4} M{1, 5} ], 'LineWidth', 2, 'EdgeColor','b');
 	
