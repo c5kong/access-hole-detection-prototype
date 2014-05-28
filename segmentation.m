a3bd20b609e2e56ff1ba540a8949b70b9ba6fa8c
ncartryerson <ncartvideos@gmail.com>
2014-05-28 10:50:05 -0400

    Score generation
    - for multiplication
    - for avg weighting

--- segmentation.m
+++ segmentation.m
@@ -374,8 +374,8 @@ function [ X ] = segmentation(frameNumber, baseDirectory)
 			detectionScore(i,1) = 0;
 		else
 			%-- assign a detection score based on the feature scores
-			%detectionScore(i,1) = (depthScore(i,1) + widthScore(i,1) + aspectRatioScore(i,1) + contrastScore(i,1) + relativeIntensityScore(i,1))/5;
-			detectionScore(i,1) = depthScore(i,1) * contrastScore(i,1) * widthScore(i,1) * aspectRatioScore(i,1) * relativeIntensityScore(i,1);
+			detectionScore(i,1) = (depthScore(i,1) + widthScore(i,1) + aspectRatioScore(i,1) + contrastScore(i,1) + relativeIntensityScore(i,1))/5;
+			%detectionScore(i,1) = depthScore(i,1) * contrastScore(i,1) * widthScore(i,1) * aspectRatioScore(i,1) * relativeIntensityScore(i,1);
 			%detectionScore(i,1) = relativeIntensityScore(i,1);
 		end
 	end	
