f942d8448e66aeb235a311201a5feb7d83ea561d
ncartryerson <ncartvideos@gmail.com>
2014-05-28 15:10:52 -0400

    new results- vary superpixels

--- segmentation.m
+++ segmentation.m
@@ -63,7 +63,7 @@ function [ X ] = segmentation(frameNumber, baseDirectory)
 	%//=======================================================================
 	%// Superpixel segmentation
 	%//=======================================================================
-	nC = 15; % nC is the target number of superpixels.
+	nC = 8; % nC is the target number of superpixels.
 	lambda_prime = .5;
 	sigma = 5.0; 
 	conn8 = 1; % flag for using 8 connected grid graph (default setting).
